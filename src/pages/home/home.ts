abcb4127f00d39747d5be62dfbe32525e83f0889
Markus Wagner <Markus Wagner>
2017-09-20 21:25:39 +0200

    added firebase

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,31 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  // pages: Array<{ title: string}>;
+  items = [
+    'Pokémon Yellow',
+    'Super Metroid',
+    'Mega Man X',
+    'The Legend of Zelda',
+    'Pac-Man',
+    'Super Mario World',
+    'Street Fighter II',
+    'Half Life',
+    'Final Fantasy VII',
+    'Star Fox',
+    'Tetris',
+    'Donkey Kong III',
+    'GoldenEye 007',
+    'Doom',
+    'Fallout',
+    'GTA',
+    'Halo'
+  ];
   constructor(public navCtrl: NavController) {
+  }
 
+  itemSelected(val:string){
+    console.log(val);
   }
 
 }
